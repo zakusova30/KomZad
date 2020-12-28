a111b67c8bdfce2b7de26ee42cf4fbce6a5f5448
Zakusova Kate <zakusova30@gmail.com>
2020-12-28 19:45:20 +0300

    Дополнения

--- KomZad/KomZad/KomZad.cpp
+++ KomZad/KomZad/KomZad.cpp
@@ -1,7 +1,9 @@
 // KomZad.cpp : Defines the entry point for the console application.
 //
-
+ 
+///////////////////////  √À¿¬Õ¿ﬂ ‘”Õ ÷»ﬂ   //////////////////////
 #include "stdafx.h"
+#include <iostream>
 
 
 int _tmain(int argc, _TCHAR* argv[])
