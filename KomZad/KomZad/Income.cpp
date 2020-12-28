ad7de16657a5b4c2422f322f32cf725c99353353
Anastasia Belyaeva <pinkipuyeeeees@gmail.com>
2020-12-29 02:08:19 +0300

    Реализация получения данных для доходов в Income.cpp

--- KomZad/KomZad/Income.cpp
+++ KomZad/KomZad/Income.cpp
@@ -1,2 +1,21 @@
 #include "stdafx.h" 
-#include "Income.h"
+#include "Income.h" 
+
+Income::Income(string i, float t, float s, string p) : id(i), priceTov(t), priceService(s), datePrice(p)
+{
+}
+// получаем ID продавца, название товара, цену и дату продажи
+string Income::getID()
+{
+	return id;
+}
+
+string Income::getDate()
+{
+	return datePrice;
+}
+
+float Income::getPriceTov()
+{
+	return priceTov;
+}
