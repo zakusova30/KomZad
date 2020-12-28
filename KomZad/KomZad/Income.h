ae474d080d35fc5a5031ebd5d4d8b914f56254f3
Anastasia Belyaeva <pinkipuyeeeees@gmail.com>
2020-12-29 01:55:48 +0300

    Добавлен класс доходов в income.h

--- KomZad/KomZad/Income.h
+++ KomZad/KomZad/Income.h
@@ -1,4 +1,19 @@
 #ifndef INCOME_H    
 #define INCOME_H   
-
+#include <string> 
+using namespace std;
+class Income //Класс доходов
+{
+private:
+	string id; //id продавца
+	float priceTov; //стоимость товаров
+	float priceService; //стоимость услуги
+	string datePrice;//дата оплаты
+public:
+	Income(string, float, float, string);
+	float getPriceTov(); //получить стоимость товара
+	string getID();      //получить ID
+	float getPriceService();
+	string getDate();
+};
 #endif
