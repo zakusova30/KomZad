fd0283813a7a66273168984ff0e87b0bb8071950
Zakusova Kate <zakusova30@gmail.com>
2020-12-28 19:47:38 +0300

    Создание класса инициализации пользователя и экран ввода данных

---
+++ KomZad/KomZad/TheFirstScreen.cpp
@@ -0,0 +1,6 @@
+/////////////////   ‘¿…À –≈¿À»«¿÷»» TheFirstScreen.h (› –¿Õ ¿¬“Œ–»«¿÷»») ////////////////
+#include "stdafx.h" 
+#include "TheFirstScreen.h" 
+#include <iostream>
+
+
---
+++ KomZad/KomZad/TheFirstScreen.h
@@ -0,0 +1,10 @@
+////////////////////////  › –¿Õ ¿¬“Œ–»«¿÷»»  ////////////////
+#pragma once
+#ifndef THEFIRSTSCREEN_H   
+#define THEFIRSTSCREEN_H
+#include "Prodavci.h"
+
+
+
+
+#endif 
