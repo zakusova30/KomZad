a20444929543b6a58317482756e4771c3d8a4a35
Sophya <sophyadergacheva@gmail.com>
2020-12-29 08:31:57 +0300

    Добавила ExpenseInputScreen.cpp - реализацию эккрана ввода расходов

---
+++ KomZad/KomZad/ExpenseInputScreen.cpp
@@ -0,0 +1,27 @@
+//// РЕАЛИЗАЦИЯ ЭКРАНА ВВОДА РАССХОДОВ ////////////////
+#include "stdafx.h" 
+#include "ExpenseInputScreen.h" 
+#include <iostream>
+using namespace std;
+
+ExpenseInputScreen::ExpenseInputScreen(ExpenseRecord* per) : ptrExpenseList(per)
+{
+}
+
+void ExpenseInputScreen::setExpense()  //экран ввода расходов
+{
+
+	string category, org, date;
+	float amount;
+	cout << "Введите категорию расходов (Цветы, цветы в горшках, водитель): ";
+	cin >> category;
+	cout << "Введите организацию (ИП Автоперевозки, ОАО Лютики): ";
+	cin >> org;
+	cout << "Введите дату платежа (20.12.2020): ";
+	cin >> date;
+	cout << "Введите сумму в рублях: ";
+	cin >> amount;
+	cin.ignore(80, '\n');
+	Expense* ptrExpense = new Expense(category, org, date, amount);
+	ptrExpenseList->insertExp(ptrExpense); // добавление новой записи в таблицу расходов
+}
