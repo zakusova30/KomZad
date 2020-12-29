eb6e6f549a9692f30e17aaee6ecb0357c652fcc2
Anastasia Belyaeva <pinkipuyeeeees@gmail.com>
2020-12-29 12:13:13 +0300

    Добавлено отображение данных годоаого отчёта на экран - AnnualReport.срр

---
+++ KomZad/KomZad/AnnualReport.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h" 
+#include "AnnualReport.h" 
+#include <iostream>
+
+AnnualReport::AnnualReport(IncomeRecord* pIN, ExpenseRecord* pER) : ptrIN(pIN), ptrER(pER)
+{
+}
+
+void AnnualReport::display()  //отображение данных на экране
+{
+	cout << " Годовой отчет" << endl;
+	cout << "-------------------------------------------------\n";
+	cout << " Доходы" << endl;
+	cout << "-------------------------------------------------\n";
+	cout << "Выручка:";
+	prof = ptrIN->getSumOfIncome();
+	cout << prof << endl;
+	cout << "-------------------------------------------------\n";
+	cout << "Расходы" << endl;
+	cout << "--------------------------------------------------\n";
+	cout << "  Категория                    |   Стоимость\n";
+	cout << "-------------------------------|------------------\n";
+	expenses = ptrER->displaySummary();
+	cout << "--------------------------------------------------\n";
+	cout << "Общие расходы:";
+	cout << expenses << endl;
+	cout << "--------------------------------------------------\n";
+	cout << "Прибыль:  " << (prof - expenses) << endl;
+	cout << "--------------------------------------------------\n";
+}
