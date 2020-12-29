50067fd60612261f5f34f9c3e02f845f7cb0893f
Anastasia Belyaeva <pinkipuyeeeees@gmail.com>
2020-12-29 13:31:08 +0300

    Добавлена реализация класса таблицы доходов - IncomeRecord.срр

---
+++ KomZad/KomZad/IncomeRecord.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h" 
+#include "IncomeRecord.h" 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+IncomeRecord::~IncomeRecord()
+{
+	while (!setPtrsIN.empty())
+	{
+		iter = setPtrsIN.begin();
+		delete *iter;
+		setPtrsIN.erase(iter);
+	}
+}
+
+
+void IncomeRecord::insertIncome(Income* ptrR) {
+	setPtrsIN.push_back(ptrR);
+}
+
+
+
+void IncomeRecord::display() //отоброжение на экране доходов                  
+{
+
+	cout << "\nID продавца        |Стоимость товара      |Стоимость услуги        |Общая стоимость         |Дата оплаты\n";
+	cout << "----------------------------------------------------------------------------------------------------------------------\n";
+	if (setPtrsIN.empty())
+		cout << "---Нет доходов---\n" << endl;
+	else
+	{
+		iter = setPtrsIN.begin();
+		while (iter != setPtrsIN.end())
+		{
+
+			cout << std::setw(15) << (*iter)->getID() << "    |     " << std::setw(15) << (*iter)->getPriceTov() << "    |     " << std::setw(15) << (*iter)->getPriceService() << "    |     " << std::setw(15) << (*iter)->getPriceTov() + (*iter)->getPriceService() << "    |     " << std::setw(15) << (*iter)->getDate() << endl;
+			*iter++;
+		}
+	}
+}
+
+float  IncomeRecord::getSumOfIncome()  //общая сумма доходов для годового отчета
+{
+	float sumIncome = 0.0;
+	iter = setPtrsIN.begin();
+	while (iter != setPtrsIN.end())
+	{
+		sumIncome += (*iter)->getPriceTov() + (*iter)->getPriceService();
+		iter++;
+	}
+	return sumIncome;
+}
+
+
